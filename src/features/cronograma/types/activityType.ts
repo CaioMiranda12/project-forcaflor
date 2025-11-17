20e4eb43c3c2fa4dd97a42c87795aaa63473d643
CaioMiranda12 <caioclm04@gmail.com>
2025-11-16 23:38:08 -0300

    feat: create activityType

---
+++ src/features/cronograma/types/activityType.ts
@@ -0,0 +1,10 @@
+export interface ActivityType {
+  id: number
+  title: string
+  description: string
+  startHour: string
+  endHour: string
+  location: string
+  instructor: string
+  dayOfWeek: string
+}
