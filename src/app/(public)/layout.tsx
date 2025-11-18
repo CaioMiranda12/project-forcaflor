68cd75b6eae23286980b911234fb7c5062d54326
CaioMiranda12 <caioclm04@gmail.com>
2025-11-18 20:44:35 -0300

    feat: add metadata on public layout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,28 @@
 import { getCurrentUser } from "@/features/auth/lib/getCurrentUser";
 import BaseLayoutClient from "@/shared/components/layout/BaseLayoutClient";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Força Flor – Portal Institucional",
+    template: "%s | Força Flor",
+  },
+  description:
+    "A Força Flor é uma OSC dedicada ao apoio social, projetos comunitários e voluntariado.",
+  keywords: [
+    "Força Flor",
+    "OSC",
+    "Organização Social",
+    "Voluntariado",
+    "Projetos sociais",
+  ],
+  openGraph: {
+    title: "Força Flor – Portal Institucional",
+    description:
+      "Conheça nossos projetos sociais, notícias e faça parte como voluntário.",
+    type: "website",
+  },
+};
 
 
 export default async function PublicLayout({ children }: { children: React.ReactNode }) {
