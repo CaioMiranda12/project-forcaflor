a4134f5dfd31871e1e4ad3994033e7f3f39d336d
CaioMiranda12 <caioclm04@gmail.com>
2025-11-18 20:44:18 -0300

    feat: add metadata on private layout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,7 +1,14 @@
 import { getCurrentUser } from "@/features/auth/lib/getCurrentUser";
 import BaseLayoutClient from "@/shared/components/layout/BaseLayoutClient";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
+export const metadata: Metadata = {
+  title: {
+    default: "Painel do Voluntário | Força Flor",
+    template: "%s | Força Flor",
+  },
+};
 
 export default async function PrivateLayout({ children }: { children: React.ReactNode }) {
   const user = await getCurrentUser();
