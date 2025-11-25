aad5425591cb0123d10891c01cacad55a998f2a0
CaioMiranda12 <caioclm04@gmail.com>
2025-11-25 17:42:51 -0300

    feat: fix format celular

--- src/shared/utils/formatCelular.ts
+++ src/shared/utils/formatCelular.ts
@@ -1,18 +1,27 @@
 export function formatCelular(value: string) {
   value = value.replace(/\D/g, "");
 
-  // limita a 11 dígitos
+  // Se apagar tudo → campo vazio
+  if (value.length === 0) return "";
+
+  // Limitar a 11 dígitos
   if (value.length > 11) value = value.slice(0, 11);
 
-  if (value.length <= 2) {
+  // Se só tiver 1 dígito, não mostrar "(" ainda
+  if (value.length === 1) {
+    return value;
+  }
+
+  // Se tiver 2 dígitos → mostra apenas "(85"
+  if (value.length === 2) {
     return `(${value}`;
   }
+
+  // (DD) XXXXX
   if (value.length <= 7) {
     return `(${value.slice(0, 2)}) ${value.slice(2)}`;
   }
-  if (value.length <= 11) {
-    return `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7)}`;
-  }
 
-  return value;
+  // (DD) XXXXX-XXXX
+  return `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7)}`;
 }
