fc0921387bc1f0125d2875a6c90fe7c1b2de89fa
CaioMiranda12 <caioclm04@gmail.com>
2025-11-25 15:24:16 -0300

    feat: use edit participant form on modal

--- src/features/participants/components/editParticipantModal.tsx
+++ src/features/participants/components/editParticipantModal.tsx
@@ -1,53 +1,10 @@
 'use client'
-import React, { useState, useEffect } from 'react'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import React, { useEffect } from 'react'
 import { z } from 'zod'
 import { Modal } from '@/features/posts/components/layout/Modal'
 import { User, Mail, Phone, MapPin, GraduationCap, Users, FileText, Calendar } from 'lucide-react'
 import { Participants } from '../types/participants'
-
-const editParticipantSchema = z.object({
-  nomeCompleto: z.string().min(3, 'Informe o nome completo'),
-  dataNascimento: z.string().regex(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d\d$/, "Formato inválido"),
-  idade: z.number().min(0),
-  sexo: z.enum(['masculino', 'feminino']),
-  endereco: z.string().min(1, 'Informe o endereço completo'),
-  bairro: z.string().min(1, 'Informe o bairro'),
-  cidade: z.string().min(1, 'Informe a cidade'),
-  estado: z.string().min(1, 'Informe o estado'),
-  cep: z.string().min(8, 'CEP inválido'),
-  telefone: z.string().optional(),
-  celular: z.string().min(8, 'Celular inválido'),
-  documento: z
-    .object({
-      rg: z.string().optional(),
-      cpf: z.string().optional(),
-      certidaoNascimento: z.string().optional(),
-    })
-    .refine((data) => data.rg || data.cpf || data.certidaoNascimento, {
-      message: 'Informe pelo menos um documento (RG, CPF ou Certidão)',
-    }),
-  escola: z.string().min(1, 'Informe a escola'),
-  serie: z.string().min(1, 'Informe o ano/série'),
-  turno: z.string().min(1, 'Informe o turno'),
-  cartaoVacina: z.string().optional(),
-  problemaSaude: z.boolean(),
-  descricaoProblema: z.string().optional(),
-  responsavel: z.object({
-    nomeCompleto: z.string().min(3, 'Informe o nome completo'),
-    email: z.string().email('Email inválido'),
-    telefone: z.string().min(8, 'Telefone inválido'),
-    endereco: z.string().min(1, 'Informe o endereço completo'),
-    rg: z.string().min(1, 'Informe o RG'),
-    cpf: z.string().min(11, 'Informe o CPF'),
-    nis: z.string().optional(),
-  }),
-  isActive: z.boolean(),
-})
-
-export type EditParticipantFormData = z.infer<typeof editParticipantSchema>
-
+import { EditParticipantFormData, useEditParticipantForm } from '../forms/edit-participant-form'
 interface EditParticipantModalProps {
   isOpen: boolean
   onClose: () => void
@@ -61,24 +18,21 @@ export function EditParticipantModal({
   onSave,
   participants,
 }: EditParticipantModalProps) {
-  const [isSubmitting, setIsSubmitting] = useState(false)
   const {
     register,
     handleSubmit,
-    formState: { errors },
-    watch,
     reset,
+    watch,
     setValue,
-  } = useForm<EditParticipantFormData>({
-    resolver: zodResolver(editParticipantSchema),
-    defaultValues: {},
-  })
+    formState: { errors, isSubmitting }
+  } = useEditParticipantForm();
+
 
   const problemaSaude = watch('problemaSaude')
   const dataNascimento = watch('dataNascimento')
 
   useEffect(() => {
-    if (participants) {
+    if (isOpen && participants) {
       setValue("isActive", participants.isActive);
       reset({
         nomeCompleto: participants.nomeCompleto,
@@ -134,14 +88,11 @@ export function EditParticipantModal({
   const onSubmit = async (data: EditParticipantFormData) => {
     if (!participants) return
 
-    setIsSubmitting(true)
     try {
       await onSave(participants.id, data)
       onClose()
     } catch (error) {
       console.error('Erro ao atualizar estudante:', error)
-    } finally {
-      setIsSubmitting(false)
     }
   }
 
