600b8d3462818ecc3c432d5c4f7524ff2cc2ca81
CaioMiranda12 <caioclm04@gmail.com>
2025-11-26 01:22:45 -0300

    feat: private participantes page only to admins

--- src/app/(private)/participantes/page.tsx
+++ src/app/(private)/participantes/page.tsx
@@ -1,8 +1,16 @@
+import { getCurrentUser } from "@/features/auth/lib/getCurrentUser";
 import { getParticipants } from "@/features/participants/actions/getParticipants";
 import ParticipantsClient from "@/features/participants/components/participantsClient";
 import { Participants } from "@/features/participants/types/participants";
+import { redirect } from "next/navigation";
 
 export default async function ParticipantsPage() {
+  const user = await getCurrentUser();
+
+  if (!user?.isAdmin) {
+    redirect("/");
+  }
+
   const participants: Participants[] = await getParticipants();
 
   return (
